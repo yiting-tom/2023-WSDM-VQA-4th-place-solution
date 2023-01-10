5c3a160c7b5b81f7138931d039ec9a942c39e05a
Yi-Ting Li <41534531+yiting-tom@users.noreply.github.com>
2023-01-10 03:05:56 -0800

    fix: ensure that formatted pickle file with correct columns

--- configs/consts.py
+++ configs/consts.py
@@ -8,3 +8,7 @@ VG_TSV_COLUMNS: List[str] = [
     'unique_id', 'image_id', 'text',
     'bbox', 'image',
 ]
+FORMTTED_PKL_COLUMNS: List[str] = [
+    'image', 'width', 'height', 'left',
+    'top', 'right', 'bottom', 'question',
+]
